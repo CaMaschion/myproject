0b2dbf79a559e86935e0bee83796b1b621319392
Camila Maschion <cm.maschion@gmail.com>
2024-02-13 16:49:34 +0000

    login screen - in progress

--- app/src/main/java/com/camila/myproject/loginscreen/LoginScreen.kt
+++ app/src/main/java/com/camila/myproject/loginscreen/LoginScreen.kt
@@ -6,7 +6,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
@@ -15,12 +17,16 @@ import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import com.camila.myproject.R
 import com.camila.myproject.ui.theme.Green80
@@ -34,6 +40,7 @@ fun LoginScreen(onLoginClicked: (String) -> Unit) {
     var enteredPassword by remember {
         mutableStateOf(TextFieldValue())
     }
+
 
     Surface(
         color = Green80
@@ -52,34 +59,42 @@ fun LoginScreen(onLoginClicked: (String) -> Unit) {
                 painter = painterResource(id = R.drawable.logo_money),
                 contentDescription = "Logo Image",
                 modifier = Modifier
-                    .size(250.dp) // Adjust the size of the image as needed
+                    .size(200.dp) // Adjust the size of the image as needed
                     .padding(bottom = 16.dp)
 
             )
 
             Spacer(modifier = Modifier.height(16.dp))
 
-            // Text Field to display username
-            TextField(
+            OutlinedTextField(
                 value = username,
                 onValueChange = { newValue ->
                     username = newValue
                 },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
                 label = { Text(text = "Nome") },
                 singleLine = true,
+                isError = false,
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    unfocusedBorderColor = Color.Black,
+                    focusedBorderColor = Color.DarkGray),
                 modifier = Modifier
                     .fillMaxWidth()
             )
 
             Spacer(modifier = Modifier.height(5.dp))
 
-            // Text Field to display entered password
-            TextField(
+            OutlinedTextField(
                 value = enteredPassword,
                 onValueChange = { enteredPassword = it },
                 visualTransformation = PasswordVisualTransformation(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
                 label = { Text(text = "Senha") },
                 singleLine = true,
+                isError = false,
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    unfocusedBorderColor = Color.Black,
+                    focusedBorderColor = Color.DarkGray),
                 modifier = Modifier
                     .fillMaxWidth()
             )
@@ -88,6 +103,10 @@ fun LoginScreen(onLoginClicked: (String) -> Unit) {
 
             TextButton(
                 onClick = { /*TODO*/ },
+                colors = ButtonDefaults.buttonColors(
+                    contentColor = Color.Black,
+                    containerColor = Green80
+                ),
                 modifier = Modifier
                     .wrapContentWidth()
                     .align(Alignment.Start)
@@ -106,18 +125,18 @@ fun LoginScreen(onLoginClicked: (String) -> Unit) {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            // Access Account Button
             Spacer(modifier = Modifier.weight(1f))
 
             Button(
                 onClick = { /*TODO*/ },
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Black),
                 modifier = Modifier
                     .padding(bottom = 16.dp)
                     .fillMaxWidth()
                     .height(60.dp),
 
                 ) {
-                Text(text = "Acesse a sua conta")
+                Text(text = "Acessar")
             }
         }
 
